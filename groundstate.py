915fcf0386f2e75a1e8da453bd70f03ae7419a3d
Jonas Leitner <j.leitner@stud.uni-heidelberg.de>
2021-12-26 09:08:29 +0100

    refactor gs energy and change __build_psi

--- groundstate.py
+++ groundstate.py
@@ -1,44 +1,51 @@
 from sympy import symbols, Rational, latex, Dummy
 from sympy.physics.secondquant import (
-    AntiSymmetricTensor, NO, F, Fd, wicks, evaluate_deltas,
-    contraction, substitute_dummies
+    AntiSymmetricTensor, NO, F, Fd, wicks, substitute_dummies
 )
 from math import factorial
 from indices import pretty_indices, indices
 
 
 class Hamiltonian:
-    def __init__(self):
-        self.__H0 = self.build_H0()
-        self.__H1 = self.build_H1()
+    def __init__(self, canonical=True):
+        self.__H0 = 0
+        self.__H1 = 0
+        self.canonical = canonical
 
-    def build_H0(self):
+    def __build_H0(self, canonical):
         p = symbols('p', cls=Dummy)
-        q = symbols('q', cls=Dummy)
-        f = AntiSymmetricTensor('f', (p,), (q,))
-        pq = Fd(p) * F(q)
-        H0 = f * pq
-        print("H0 = ", latex(H0))
-        return H0
+        if canonical:
+            f = AntiSymmetricTensor('f', (p,), (p,))
+            pp = Fd(p) * F(p)
+            self.__H0 = f * pp
+        else:
+            q = symbols('q', cls=Dummy)
+            f = AntiSymmetricTensor('f', (p,), (q,))
+            pq = Fd(p) * F(q)
+            self.__H0 = f * pq
+        print("H0 = ", latex(self.__H0))
 
     @property
     def get_H0(self):
+        if not self.__H0:
+            self.__build_H0(self.canonical)
         return self.__H0
-
-    def build_H1(self):
-        p, q, r, s = symbols('p,q,r,s', cls=Dummy)
-        m = symbols('m', below_fermi=True, cls=Dummy)
-        v1 = AntiSymmetricTensor('V', (p, m), (q, m))
-        pq = Fd(p) * F(q)
-        v2 = AntiSymmetricTensor('V', (p, q), (r, s))
-        pqsr = Fd(p) * Fd(q) * F(s) * F(r)
-        H1 = -v1 * pq + Rational(1, 4) * v2 * pqsr
-        print("H1 = ", latex(H1))
-        return H1.expand()
 
     @property
     def get_H1(self):
+        if not self.__H1:
+            self.__build_H1()
         return self.__H1
+
+    def __build_H1(self):
+        p, q, r, s = symbols('p,q,r,s', cls=Dummy)
+        o1 = symbols('o1', below_fermi=True, cls=Dummy)
+        v1 = AntiSymmetricTensor('V', (p, o1), (q, o1))
+        pq = Fd(p) * F(q)
+        v2 = AntiSymmetricTensor('V', (p, q), (r, s))
+        pqsr = Fd(p) * Fd(q) * F(s) * F(r)
+        self.__H1 = -v1 * pq + Rational(1, 4) * v2 * pqsr
+        print("H1 = ", latex(self.__H1))
 
 
 class ground_state:
@@ -49,25 +56,48 @@ class ground_state:
         self.wfn = {}
 
     def get_energy(self, order):
-        if not self.energy.get(order, False):
-            try:
-                callback = getattr(self, "build_E" + str(order))
-                callback()
-            except AttributeError:
-                print(f"Ground state energy of order {order} not implemented.")
-                exit()
+        """Returns the ground state energy of specified order."""
+
+        if not isinstance(order, int):
+            print("Order for obtaining ground state wavefunction/energy",
+                  f"needs to of type int. {type(order)} is not valid.")
+            exit()
+
+        if order not in self.energy:
+            self.__build_E(order)
         return self.energy[order]
+
+    def __build_E(self, order):
+        def H(o): return self.h.get_H0 if o == 0 else self.h.get_H1
+        h = H(order)
+        bra = self.get_psi(0, "bra")
+        def Ket(o): return self.get_psi(0, "ket") if o == 0 else \
+            self.get_psi(order-1, "ket")
+        ket = Ket(order)
+        e = bra * h * ket
+        e = wicks(e, keep_only_fully_contracted=True,
+                  simplify_kronecker_deltas=True)
+        self.energy[order] = e
+        print(f"E^({order}) = {latex(e)}")
+
+    def get_pretty_energy(self, order):
+        return substitute_dummies(self.get_energy(order), new_indices=True,
+                                  pretty_indices=pretty_indices)
 
     def get_psi(self, order, braket):
         """Retuns the ground state wavefunction of the requested order.
-           The type (Bra or Ket) of the requested wavefunction may be
-           provided as ket=True and/or bra=True.
-           Returns a dict
-           By default only the Ket is build and returned."""
+           The type (bra or ket) of the requested wavefunction needs to
+           be provided as str.
+           """
 
+        if not isinstance(order, int):
+            print("Order for obtaining ground state wavefunction/energy",
+                  f"needs to of type int. {type(order)} is not valid.")
+            exit()
         if braket not in ["ket", "bra"]:
             print("Only possible to build 'bra' or 'ket' gs wavefunction",
                   f"{braket} is not valid")
+            exit()
 
         if not self.wfn.get(order, False):
             self.wfn[order] = {}
@@ -80,88 +110,36 @@ class ground_state:
                 self.__build_psi(order, braket)
         return self.wfn[order][braket]
 
-    def build_E0(self):
-        p, q = symbols('p,q', cls=Dummy)
-        # instead use function... does it make any difference?
-        f = AntiSymmetricTensor('f', (p,), (q,))
-        pq = contraction(Fd(p), F(q))
-        E0 = f * pq
-        E0 = evaluate_deltas(E0)
-        self.energy[0] = E0
-        print("E0 = ", latex(E0))
-
-    def build_E1(self):
-        # for some reason wicks does not work here... produces 0
-        # Therefore, it is necessary to compute the contractions
-        # manually. However, the manual contraction only returns
-        # "a" or "i" as indices when general symbols (p, q...)
-        # are used. Therefore, occupied indices needed to be used.
-
-        # p, q, r, s = symbols('p,q,r,s', cls=Dummy)
-        p, q, r, s = symbols('i,j,k,l', below_fermi=True, cls=Dummy)
-        pq = contraction(Fd(p), F(q))
-        v1 = AntiSymmetricTensor('V', (p, r), (q, r))
-        v2 = AntiSymmetricTensor('V', (p, q), (r, s))
-        pqsr = + contraction(Fd(p), F(r)) * contraction(Fd(q), F(s)) \
-            - contraction(Fd(p), F(s)) * contraction(Fd(q), F(r))
-        E1 = Rational(1, 4) * v2 * pqsr - v1 * pq
-        E1 = evaluate_deltas(E1.expand())
-        E1 = substitute_dummies(E1)
-        print("E1 = ", latex(E1))
-        self.energy[1] = E1
-
-    def build_E2(self):
-        psi1 = self.get_psi(1, ket=True)
-        psi0 = self.get_psi(0, bra=True)
-        H1 = self.h.get_H1
-        E2 = psi0["bra"] * H1 * psi1["ket"]
-        E2 = wicks(E2, keep_only_fully_contracted=True)
-        E2 = evaluate_deltas(E2)
-        E2 = substitute_dummies(
-            E2, new_indices=True, pretty_indices=pretty_indices
-        )
-        print("E2 = ", latex(E2))
-        self.energy[2] = E2
-
-    def __build_psi(self, order, *args):
+    def __build_psi(self, order, braket):
         # generalize the gs wavefunction generation
         # currently only used for second oder +
-        if "bra" in args:
-            psi = 0
-            for excitation in range(1, 2 * order + 1):
-                idx = self.indices.get_gs_indices(
-                    "bra", n_occ=excitation, n_virt=excitation
-                )
-                t = AntiSymmetricTensor(
-                    f"t{order}_cc", tuple(idx["virt"]), tuple(idx["occ"])
-                )
-                operators = 1
-                for symbol in idx["occ"]:
-                    operators *= Fd(symbol)
-                for symbol in reversed(idx["virt"]):
-                    operators *= F(symbol)
-                prefactor = Rational(1, factorial(excitation) ** 2)
-                psi += prefactor * t * NO(operators)
-            self.wfn[order]["bra"] = psi
-            print(f"<Psi^({order})| = ", latex(psi))
-        if "ket" in args:
-            psi = 0
-            for excitation in range(1, 2 * order + 1):
-                idx = self.indices.get_gs_indices(
-                    "ket", n_occ=excitation, n_virt=excitation
-                )
-                t = AntiSymmetricTensor(
-                    f"t{order}", tuple(idx["virt"]), tuple(idx["occ"])
-                )
-                operators = 1
-                for symbol in idx["virt"]:
-                    operators *= Fd(symbol)
-                for symbol in reversed(idx["occ"]):
-                    operators *= F(symbol)
-                prefactor = Rational(1, factorial(excitation) ** 2)
-                psi += prefactor * t * NO(operators)
-            self.wfn[order]["ket"] = psi
-            print(f"|Psi^({order})> = {latex(psi)}\n\n")
+        tensor_string = {
+            "bra": f"t{order}_cc",
+            "ket": f"t{order}"
+        }
+        get_ov = {
+            "bra": lambda ov: [other for other in ["occ", "virt"]
+                               if other != ov],
+            "ket": lambda ov: ov
+        }
+        psi = 0
+        for excitation in range(1, 2 + order + 1):
+            idx = self.indices.get_gs_indices(
+                braket, n_occ=excitation, n_virt=excitation
+            )
+            t = AntiSymmetricTensor(
+                tensor_string[braket], tuple(idx["virt"]), tuple(idx["occ"])
+            )
+            operators = 1
+            for symbol in idx["".join(get_ov[braket]('virt'))]:
+                operators *= Fd(symbol)
+            for symbol in reversed(idx["".join(get_ov[braket]('occ'))]):
+                operators *= F(symbol)
+            # prefactor correct?
+            prefactor = Rational(1, factorial(excitation) ** 2)
+            psi += prefactor * t * NO(operators)
+        self.wfn[order][braket] = psi
+        print(f"Build gs^({order}) {braket} = ", latex(psi))
 
     def _build_psi0(self, *args):
         for braket in args:
@@ -172,6 +150,7 @@ class ground_state:
                 print(f"<Psi^(0)| = {self.wfn[0][braket]}\n\n")
 
     def _build_psi1(self, *args):
+        # special function for psi1, because singles are excluded atm.
         if "bra" in args:
             idx = self.indices.get_gs_indices("bra", n_occ=2, n_virt=2)
             t = AntiSymmetricTensor(
@@ -202,11 +181,11 @@ class ground_state:
             print(f"|Psi^(1)>  = {latex(psi)}\n\n")
 
 
-# h = Hamiltonian()
+# h = Hamiltonian(canonical=False)
 # h.get_H0
 # h.get_H1
 # mp = ground_state(h)
-# a = mp.get_energy(order=0)
-# a = mp.get_energy(order=1)
-# a = mp.get_energy(order=2)
-# a = mp.get_psi(order=2, ket=True, bra=True)
+# mp.indices.invoke_space("ph")
+# a = mp.get_pretty_energy(2)
+# print(latex(a))
+# a = mp.get_psi(2, 'bra')
