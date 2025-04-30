90adc498270e873e358140593b107300cd856e04
jonasleitner <jonas.leitner1@gmx.de>
2025-04-30 12:32:19 +0200

    port derivative

--- adcgen/derivative.py
+++ adcgen/derivative.py
@@ -1,23 +1,21 @@
-from . import expr_container as e
+from sympy import Mul, Rational, S, diff
+
+from .expression import ExprContainer, ObjectContainer
 from .indices import minimize_tensor_indices, Index
-from sympy import Rational, diff, S
+from .sympy_objects import SymbolicTensor
 
 
-def derivative(expr: e.Expr, t_string: str):
+def derivative(expr: ExprContainer, t_string: str
+               ) -> dict[tuple[str, str], ExprContainer]:
     """Computes the derivative of an expression with respect to a tensor.
        The derivative is separated block whise, e.g, terms that contribute to
        the derivative w.r.t. the oooo ERI block are separated from terms that
        contribute to the ooov block.
        Assumptions of the input expression are NOT updated or modified.
        The derivative is NOT simplified."""
-
-    if not isinstance(t_string, str):
-        raise TypeError("Tensor name needs to be provided as str.")
-
+    assert isinstance(t_string, str)
+    assert isinstance(expr, ExprContainer)
     expr = expr.expand()
-    if not isinstance(expr, e.Expr):  # ensure expr is in a container
-        expr = e.Expr(expr)
-
     # create some Dummy Symbol. Replace the tensor with the Symbol and
     # compute the derivative with respect to the Symbol. Afterwards
     # resubstitute the Tensor for the Dummy Symbol.
@@ -28,8 +26,8 @@ def derivative(expr: e.Expr, t_string: str):
         assumptions = term.assumptions
         objects = term.objects
         # - find all occurences of the desired tensor
-        tensor_obj = []
-        remaining_obj = e.Expr(1, **term.assumptions)
+        tensor_obj: list[ObjectContainer] = []
+        remaining_obj = ExprContainer(1, **term.assumptions)
         for obj in objects:
             if obj.name == t_string:
                 tensor_obj.append(obj)
@@ -37,7 +35,7 @@ def derivative(expr: e.Expr, t_string: str):
                 remaining_obj *= obj
 
         # - extract the names of target indices of the term
-        target_names_by_space = {}
+        target_names_by_space: dict[tuple[str, str], set[str]] = {}
         for s in term.target:
             if (key := s.space_and_spin) not in target_names_by_space:
                 target_names_by_space[key] = set()
@@ -58,7 +56,7 @@ def derivative(expr: e.Expr, t_string: str):
             _, perms = minimize_tensor_indices(obj.idx, target_names_by_space)
             # - apply the permutations to the remaining term
             deriv_contrib = deriv_contrib.permute(*perms)
-            if deriv_contrib.sympy is S.Zero:
+            if deriv_contrib.inner is S.Zero:
                 raise RuntimeError(f"Mnimization permutations {perms} let "
                                    f"the remaining term {deriv_contrib} "
                                    "vanish.")
@@ -66,8 +64,8 @@ def derivative(expr: e.Expr, t_string: str):
             #   a prefactor of -1 that we need to move to the deriv_contrib.
             #   Also the indices might be further minimized due to the
             #   symmetry of the tensor obj
-            obj: e.Term = obj.permute(*perms).terms[0]
-            if (factor := obj.prefactor) < 0:
+            obj = obj.permute(*perms).terms[0]
+            if (factor := obj.prefactor) < S.Zero:
                 deriv_contrib *= factor
             # - Apply the symmetry of the removed tensor to the remaining
             #   term to ensure that the result has the correct symmetry.
@@ -75,27 +73,31 @@ def derivative(expr: e.Expr, t_string: str):
             #   This allows to compute the symbolic derivative with diff.
             tensor_sym = obj.symmetry()
             deriv_contrib *= Rational(1, len(tensor_sym) + 1)
-            symmetrized_deriv_contrib = deriv_contrib.sympy * x**exponent
+            symmetrized_deriv_contrib = S.Zero
+            symmetrized_deriv_contrib += Mul(deriv_contrib.inner, x**exponent)
             for perms, factor in tensor_sym.items():
-                symmetrized_deriv_contrib += (
-                    deriv_contrib.copy().permute(*perms).sympy *
-                    factor * x**exponent
+                symmetrized_deriv_contrib += Mul(
+                    deriv_contrib.copy().permute(*perms).inner,
+                    factor,
+                    x**exponent
                 )
             # - compute the derivative with respect to x
             symmetrized_deriv_contrib = diff(symmetrized_deriv_contrib, x)
             # - replace x by the removed tensor (due to diff the exponent
             #   is lowered by 1)
-            obj = obj.tensors
+            obj = [
+                o for o in obj.objects if isinstance(o.base, SymbolicTensor)
+            ]
             assert len(obj) == 1
             obj = obj[0]
             symmetrized_deriv_contrib = (
-                symmetrized_deriv_contrib.subs(x, obj)
+                symmetrized_deriv_contrib.subs(x, obj.base)
             )
             # - sort the derivative according to the space of the minimal
             #   tensor indices
             #   -> sort the derivative block whise.
             key = (obj.space, obj.spin)
             if key not in derivative:
-                derivative[key] = e.Expr(0, **assumptions)
+                derivative[key] = ExprContainer(0, **assumptions)
             derivative[key] += symmetrized_deriv_contrib
     return derivative
