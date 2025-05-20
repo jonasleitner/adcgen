1933098e1ab1c2363fcfe44c88dd6886d976d657
LinusBDittmer <ldittmer@github.com>
2025-05-20 12:34:55 +0200

    Removed build folder

--- build/lib/adcgen/generate_code/__init__.py
+++
@@ -1,8 +0,0 @@
-from .contraction import Contraction
-from .generate_code import generate_code
-from .optimize_contractions import (
-    optimize_contractions, unoptimized_contraction
-)
-
-__all__ = ["Contraction", "generate_code", "optimize_contractions",
-           "unoptimized_contraction"]
